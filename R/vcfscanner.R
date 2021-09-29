e4fc27b35e3bee63f950a4b61f22c420ef1716cb
Fréderique Boeykens <frederiqueboeykens@w245h216.wireless.ugent.be>
2021-09-29 17:02:01 +0200

    First commit

---
+++ R/vcfscanner.R
@@ -0,0 +1,32 @@
+#' @title vcfscanner
+#' @description Uploads a single sample VCF file (from a multi sample VCF file) into R.
+#'
+#' @param x Character string containing the name of the vcf file or file path.
+#' @param sample Character string containing the name of the sample of interest. The sample name has to match with that of the VCF file.
+#'
+#' @return Returns a single sample vcf file ready for downstream analysis.
+#' @export
+#' @importFrom vcfR read.vcfR
+#' @import dplyr
+#'
+#'@examples
+#'\donttest{}
+#'#example
+#'\donttest{}
+#'
+
+
+vcfscanner <- function(x, sample){
+  vcf <- vcfR::read.vcfR(x)
+  fix <- vcf@fix
+  fix <- as.data.frame(fix, stringsAsFactors = FALSE)
+  gt <- vcf@gt
+  gt <- as.data.frame(gt, stringsAsFactors = FALSE)
+  fix <- cbind(fix, gt[,1])
+  names(fix)[9] <- "FORMAT"
+  gt <- gt %>% dplyr::select(all_of(sample))
+  vcf <- cbind.data.frame(fix,gt)
+  vcf_names <- c("chrom", "pos", "id", "ref", "alt", "qual", "filter", "info", "format", "sample")
+  names(vcf) <- vcf_names
+  return(vcf)
+}
